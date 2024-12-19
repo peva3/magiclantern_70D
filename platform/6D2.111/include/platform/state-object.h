4c84abb92e78089dd5010fae94d99e132a26600f
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2024-12-19 07:58:20 +0000

    6d2: fix some state object addresses
    Don't think these are used presently, but I happened to notice
    they were wrong.

--- platform/6D2.111/include/platform/state-object.h
+++ platform/6D2.111/include/platform/state-object.h
@@ -1,9 +1,7 @@
 #ifndef __platform_state_object_h
 #define __platform_state_object_h
 
-#define MOVREC_STATE (*(struct state_object **)0x6CDC)
-#define LV_STATE (*(struct state_object **)0x4580)
-//~ #define SDS_FRONT_1 (*(struct state_object **)0x374C)
-#define SDS_FRONT3_STATE (*(struct state_object **)0x3750)
+#define MOVREC_STATE (*(struct state_object **)0xf470)
+#define EVF_STATE (*(struct state_object **)0x60fc)
 
 #endif // __platform_state_object_h
