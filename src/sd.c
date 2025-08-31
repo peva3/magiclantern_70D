e2c64b21834772d705527b85d044b5de37cf4f25
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2025-09-01 00:43:45 +0100

    200D: fix 192Mhz UHS detection
    It appears to be a Canon bug that their autotuning SD UHS
    code skips the first entry; 192MHz.  Patching the table
    of mode indices so that 192 is second makes it work.

--- src/sd.c
+++ src/sd.c
@@ -15,6 +15,48 @@
 
 #ifdef FEATURE_SD_AUTOTUNE
 
+#if defined(CONFIG_MMU_REMAP) && defined(CONFIG_200D)
+#include "patch.h"
+static int is_patched = 0;
+
+#if !defined(CONFIG_FW_VERSION) || CONFIG_FW_VERSION != 101
+    #error "The mode_192 addresses for patching have only been found for fw 1.0.1.  You will need to validate them."
+#endif
+
+struct patch mode_192[] =
+{
+// 200D has code to autotune SD speed, but there seems to be a bug.
+// There's an array which holds an index for selecting speed,
+// out of a set starting 192Mhz, then 156, 130, 111.
+// They loop over the possible speeds, but they increment the index
+// at the top of the loop - the first possible speed is skipped.
+// Thus it will never select higher than 156Mhz.
+//
+// We patch this table so it selects from 192, 156, 111.
+// (even pretty old cards can do 156, but we keep the slowest "fast"
+// speed as a fallback).
+
+// e0ea7eb8 - this stores 192MHz mode index; 9, but it's never used (looks like a bug)
+// e0ea7ebc - this stores 156Mhz mode index; 8, which is checked first
+// e0ea7ec0 - this stores 130Mhz mode index; 7, checked second
+
+    {
+        .addr = (uint8_t *)0xe0ea7ebc,
+        .old_value = 0x8,
+        .new_value = 0x9,
+        .size = 4,
+        .description = "Allow 192MHz SD speed (156)"
+    },
+    {
+        .addr = (uint8_t *)0xe0ea7ec0,
+        .old_value = 0x7,
+        .new_value = 0x8,
+        .size = 4,
+        .description = "Allow 192MHz SD speed (130)"
+    },
+};
+#endif // CONFIG_MMU_REMAP && CONFIG_200D
+
 static CONFIG_INT("SD.enable_autotune_on_boot", is_autotune_enabled, 0);
 
 // Wraps DryOS function for ML.
@@ -32,7 +74,16 @@ static void run_SD_autotune(void *priv_unused, int unused)
     is_autotune_enabled = !is_autotune_enabled;
 
     if (is_autotune_enabled)
+    {
+        #if defined(CONFIG_MMU_REMAP) && defined(CONFIG_200D)
+        if (!is_patched)
+        {
+            apply_patches(mode_192, COUNT(mode_192));
+            is_patched = 1;
+        }
+        #endif
         autotune_SD();
+    }
 }
 
 static struct menu_entry autotune_SD_speed_menu[] = {
@@ -57,6 +108,13 @@ static void autotune_SD_task()
         msleep(1500); // Don't run during early boot while OS is still configuring SD.
                       // That seems to cause hangs.
                       // This is running as a task so sleeping is fine.
+        #if defined(CONFIG_MMU_REMAP) && defined(CONFIG_200D)
+        if (!is_patched)
+        {
+            apply_patches(mode_192, COUNT(mode_192));
+            is_patched = 1;
+        }
+        #endif
         extern void autotune_SD(void);
         autotune_SD();
     }
